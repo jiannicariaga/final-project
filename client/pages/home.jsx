198bc0220c7066cd1e36595e8ae4a382b687d549
Jianni Cariaga <jiannicariaga@gmail.com>
2022-11-09 06:59:37 +0000

    Converted Home component to class and added getData method. #2

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,11 +3,25 @@ import Container from 'react-bootstrap/Container';
 import SearchForm from '../components/search-form';
 import Definition from '../components/definition';
 
-export default function Home(props) {
-  return (
-    <Container>
-      <SearchForm />
-      <Definition />
-    </Container>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData(formInputs) {
+
+  }
+
+  render() {
+    return (
+      <Container>
+        <SearchForm getData={this.getData} />
+        <Definition />
+      </Container>
+    );
+  }
 }
