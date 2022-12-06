580dc4a25449c117a39e891d23dbdae76807b146
Jianni Cariaga <jiannicariaga@gmail.com>
2022-12-06 01:48:56 +0000

    Updated error message. #22

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -4,7 +4,7 @@ const ClientError = require('./client-error');
 
 function authorizationMiddleware(req, res, next) {
   const token = req.get('X-Access-Token');
-  if (!token) throw new ClientError(401, 'Authorization required.');
+  if (!token) throw new ClientError(401, 'authorization required.');
   const payload = jwt.verify(token, process.env.TOKEN_SECRET);
   req.user = payload;
   next();
