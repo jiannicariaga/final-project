8bf89c2e35be2108e51eb024b79fd554b033179d
Jianni Cariaga <jiannicariaga@gmail.com>
2022-12-06 20:05:25 +0000

    Addressed non-collapsing sidebar on screen resize.

--- client/lib/parse-route.js
+++ client/lib/parse-route.js
@@ -1,7 +1,5 @@
 export default function parseRoute(hashRoute) {
-  if (hashRoute.startsWith('#')) {
-    hashRoute = hashRoute.replace('#', '');
-  }
+  if (hashRoute.startsWith('#')) hashRoute = hashRoute.replace('#', '');
   const [path, queryString] = hashRoute.split('?');
   const params = new URLSearchParams(queryString);
   return { path, params };
